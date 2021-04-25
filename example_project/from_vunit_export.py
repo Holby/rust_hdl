ab96d2303eacdbe4993380804a500fcad96ed179
Holby <bjorn.holby@gmail.com>
2021-04-25 17:18:40 +0200

    Create project files for ghdl-ls and hdl-checker

--- example_project/from_vunit_export.py
+++ example_project/from_vunit_export.py
@@ -15,6 +15,8 @@ def main():
                         help="The input .json file")
     parser.add_argument("-o", "--output", default="vhdl_ls.toml",
                         help="The output vhdl_ls.toml file")
+    parser.add_argument("-g", "--ghdl-lib", default="lib",
+                        help="Which lib to use for ghdl-ls")
 
     args = parser.parse_args()
 
@@ -22,14 +24,20 @@ def main():
         data = json.load(fptr)
 
     libraries = {}
+    hdl_checker = {'sources': []}
+    ghdl = {'options': {'ghdl_analysis': ["--std=08"]}, 'files': []}
     for source_file in data["files"]:
         file_name = source_file["file_name"]
         library_name = source_file["library_name"]
 
         if not library_name in libraries:
             libraries[library_name] = set()
+            ghdl['options']['ghdl_analysis'].append("-Pvunit_out/ghdl/libraries/{}".format(library_name))
 
         libraries[library_name].add(file_name)
+        hdl_checker['sources'].append([file_name, {'library': library_name, 'flags': ["--std=08"]}])
+        if library_name == args.ghdl_lib:
+            ghdl['files'].append({"file": file_name, "language": "vhdl"})
 
     with open(args.output, "w") as fptr:
         for key in libraries:
@@ -37,7 +45,12 @@ def main():
                                          for file_name in libraries[key]])
         toml.dump(dict(libraries=libraries), fptr)
 
+    with open(".hdl_checker.config", "w") as outfile:
+        json.dump(hdl_checker, outfile, indent=4)
 
+    with open("hdl-prj.json", "w") as outfile:
+        json.dump(ghdl, outfile, indent=4)
+        
 
 if __name__ == "__main__":
     main()
